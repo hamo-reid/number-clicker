06e7b5e1397f8f96613e990b783a5a41bff31061
hamo-reid <190395489@qq.com>
2025-04-29 21:33:57 +0800

    🩹: fix workflows 🩹: fix vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/number-clicker/',
   plugins: [vue()],
   server: {
     port: 13000
+  },
+  build: {
+    outDir: 'dist',
   }
 })
